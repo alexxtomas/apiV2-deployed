77c69a55b24e66fb874da3114a1d933992a5c25d
Alex Tomas <atomasllimos@gmail.com>
2023-01-01 10:57:42 +0100

    feat: Seeding db

--- src/server/app.ts
+++ src/server/app.ts
@@ -15,7 +15,6 @@ app.set('port', process.env.PORT || 3001)
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-
 // Routes
 app.use('/api/auth', authRoutes)
 app.use('/api/restaurants', restaurantsRoutes)
